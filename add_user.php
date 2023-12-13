3679e3ad13152e633750a0e6c53a8ad66089f995
Eduard-Halay <128895997+Eduard-Halay@users.noreply.github.com>
2023-12-13 15:47:25 +0200

    Update add_user.php

--- add_user.php
+++ add_user.php
@@ -7,7 +7,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $role = $_POST['role'];
     $status = $_POST['status'];
 
-     $query = "INSERT INTO `client` (`name`, `lastname`, `role`, `status`) VALUES ('$name', '$lastname', '$role', '$status')";
+    $query = "INSERT INTO `client` (`name`, `lastname`, `role`, `status`) VALUES ('$name', '$lastname', '$role', '$status')";
     mysqli_query($induction, $query);
+
+    // Получение данных нового пользователя
+    $newUserId = mysqli_insert_id($induction);
+    $result = mysqli_query($induction, "SELECT * FROM `client` WHERE `id` = $newUserId");
+    $newUserData = mysqli_fetch_assoc($result);
+
+    // Возвращение данных в формате JSON
+    header('Content-Type: application/json');
+    echo json_encode($newUserData);
 }
 ?>
