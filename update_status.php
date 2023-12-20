a590f739e55ad66c28ea62c0eea6487b565be055
Eduard-Halay <128895997+Eduard-Halay@users.noreply.github.com>
2023-12-20 19:59:19 +0200

    new update_status.php

--- update_status.php
+++ update_status.php
@@ -5,20 +5,28 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $userId = $_POST['userId'];
     $newStatus = $_POST['newStatus'];
 
-   
-    $updateQuery = "UPDATE `client` SET `status`='$newStatus' WHERE `id`='$userId'";
-    mysqli_query($induction, $updateQuery);
-
- 
-    $selectQuery = "SELECT `status` FROM `client` WHERE `id`='$userId'";
-    $result = mysqli_query($induction, $selectQuery);
-    $row = mysqli_fetch_assoc($result);
-
-  
-    echo json_encode(['status' => $row['status']]);
-} else {
     
+    $updateQuery = "UPDATE `client` SET `status`='$newStatus' WHERE `id`='$userId'";
+    
+   
+    if (mysqli_query($induction, $updateQuery)) {
+       
+        $selectQuery = "SELECT `status` FROM `client` WHERE `id`='$userId'";
+        $result = mysqli_query($induction, $selectQuery);
+        
+       
+        $row = mysqli_fetch_assoc($result);
+        
+        
+        echo json_encode(['status' => $row['status']]);
+    } else {
+       
+        http_response_code(500); 
+        echo json_encode(['error' => ['code' => 105, 'message' => 'Failed to update status']]);
+    }
+} else {
+   
     http_response_code(400);
-    echo json_encode(['error' => '{status: false, error:{code: 105, message: "Invalid request method"}}']);
+    echo json_encode(['error' => ['code' => 105, 'message' => 'Invalid request method']]);
 }
 ?>
