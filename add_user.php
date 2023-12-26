999f1facd96810aaa2c92120a4fed497f921921e
Eduard-Halay <128895997+Eduard-Halay@users.noreply.github.com>
2023-12-26 15:51:35 +0200

    fix net

--- add_user.php
+++ add_user.php
@@ -8,15 +8,36 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $status = $_POST['status'];
 
     $query = "INSERT INTO `client` (`name`, `lastname`, `role`, `status`) VALUES ('$name', '$lastname', '$role', '$status')";
-    mysqli_query($induction, $query);
+    $insertResult = mysqli_query($induction, $query);
 
-    // Получение данных нового пользователя
-    $newUserId = mysqli_insert_id($induction);
-    $result = mysqli_query($induction, "SELECT * FROM `client` WHERE `id` = $newUserId");
-    $newUserData = mysqli_fetch_assoc($result);
+    if ($insertResult) {
+       
+        $newUserId = mysqli_insert_id($induction);
+        $result = mysqli_query($induction, "SELECT * FROM `client` WHERE `id` = $newUserId");
+        $newUserData = mysqli_fetch_assoc($result);
 
-    // Возвращение данных в формате JSON
+        
+        $response = array(
+            'statusOperation' => true,
+            'error' => null,
+            'id' => $newUserData['id'],
+            'name' => $newUserData['name'],
+            'lastname' => $newUserData['lastname'],
+            'role' => $newUserData['role'],
+            'status' => $newUserData['status']
+        );
+    } else {
+      
+        $response = array(
+            'statusOperation' => false,
+            'error' => array('code' => 201, 'message' => 'Error inserting user data')
+        );
+    }
+
+    
     header('Content-Type: application/json');
-    echo json_encode($newUserData);
+
+   
+    echo json_encode($response);
 }
 ?>
