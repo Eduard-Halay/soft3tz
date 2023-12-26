780dd9387237e9cb52f5b1ca7bc261a47a44f0de
Eduard-Halay <128895997+Eduard-Halay@users.noreply.github.com>
2023-12-26 15:50:16 +0200

    fix net

--- update_user.php
+++ update_user.php
@@ -8,27 +8,64 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $status = $_POST['status'];
     $role = intval($_POST['role']);
 
-    
     $updateQuery = "UPDATE `client` SET `name`='$name', `lastname`='$lastname', `status`='$status', `role`='$role' WHERE `id`='$userId'";
     $updateResult = mysqli_query($induction, $updateQuery);
 
     if ($updateResult) {
-       
         $selectQuery = "SELECT * FROM `client` WHERE `id`='$userId'";
         $selectResult = mysqli_query($induction, $selectQuery);
 
         if ($selectResult) {
             $userData = mysqli_fetch_assoc($selectResult);
 
-           header('Content-Type: application/json');
-            echo json_encode($userData);
+            
+            if ($userData === null) {
+                $errorResponse = array(
+                    'statusOperation' => false,
+                    'error' => array('code' => 104, 'message' => 'Object is deleted')
+                );
+                echo json_encode($errorResponse);
+                exit;
+            }
+
+           
+            $response = array(
+                'statusOperation' => true,
+                'error' => null,
+                'id' => $userId,
+                'name' => $userData['name'],
+                'lastname' => $userData['lastname'],
+                'status' => $userData['status'],
+                'role' => $userData['role']
+            );
+
+         
+            header('Content-Type: application/json');
+
+            
+            echo json_encode($response);
         } else {
-            echo '{status: false, error:{code: 101, message: "Error selecting user"}} . mysqli_error($induction)';
+          
+            $errorResponse = array(
+                'statusOperation' => false,
+                'error' => array('code' => 101, 'message' => 'Error selecting user')
+            );
+            echo json_encode($errorResponse);
         }
     } else {
-        echo '{status: false, error:{code: 102, message: "Error updating user"}} . mysqli_error($induction)';
+       
+        $errorResponse = array(
+            'statusOperation' => false,
+            'error' => array('code' => 102, 'message' => 'Error updating user')
+        );
+        echo json_encode($errorResponse);
     }
 } else {
-    echo '{status: false, error:{code: 103, message: "Invalid request"}}';
+   
+    $errorResponse = array(
+        'statusOperation' => false,
+        'error' => array('code' => 103, 'message' => 'Invalid request')
+    );
+    echo json_encode($errorResponse);
 }
 ?>
