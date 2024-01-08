03c91655f3c7241f78364740d7fa84af4f04bfe2
Eduard-Halay <128895997+Eduard-Halay@users.noreply.github.com>
2024-01-08 14:42:22 +0200

    alert user

--- update_status.php
+++ update_status.php
@@ -2,33 +2,37 @@
 include "databases.php";
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $users = $_POST['users'];
-    $newStatus = $_POST['newStatus'];
+    $users = isset($_POST['users']) ? $_POST['users'] : [];
+    $newStatus = isset($_POST['newStatus']) ? $_POST['newStatus'] : '';
 
     $successResponse = ['status' => true, 'error' => null, 'users' => []];
     $errors = [];
 
     foreach ($users as $userData) {
-        $userId = $userData['id'];
-        $updateQuery = "UPDATE `client` SET `status`='$newStatus' WHERE `id`='$userId'";
+        $userId = isset($userData['id']) ? $userData['id'] : null;
 
-        if (mysqli_query($induction, $updateQuery)) {
-            $selectQuery = "SELECT `status` FROM `client` WHERE `id`='$userId'";
-            $result = mysqli_query($induction, $selectQuery);
+        if ($userId !== null) {
+            $updateQuery = "UPDATE `client` SET `status`='$newStatus' WHERE `id`='$userId'";
 
-            if ($result) {
-                $row = mysqli_fetch_assoc($result);
-                $status = $row['status'];
+            if (mysqli_query($induction, $updateQuery)) {
+                $selectQuery = "SELECT `status` FROM `client` WHERE `id`='$userId'";
+                $result = mysqli_query($induction, $selectQuery);
 
-                if ($row) {
-                    $successResponse['users'][] = ['id' => $userId, 'status' => $status];
-                } else {
-                  
-                    $errors[] = ['userId' => $userId, 'error' => 'User with id ' . $userId . ' not found'];
+                if ($result) {
+                    $row = mysqli_fetch_assoc($result);
+
+                    if ($row) {
+                        $status = $row['status'];
+                        $successResponse['users'][] = ['id' => $userId, 'status' => $status];
+                    } else {
+                        $errors[] = ['userId' => $userId, 'error' => 'User with id ' . $userId . ' not found'];
+                    }
                 }
+            } else {
+                $errors[] = ['userId' => $userId, 'error' => 'Error updating status'];
             }
         } else {
-            $errors[] = ['userId' => $userId, 'error' => 'Error updating status'];
+            $errors[] = ['error' => 'User id is missing'];
         }
     }
 
