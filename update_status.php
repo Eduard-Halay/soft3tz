9fa9465eb5b5d7a71f0e28d34ec4599ccfcbeb8d
Eduard-Halay <128895997+Eduard-Halay@users.noreply.github.com>
2024-01-10 17:36:14 +0200

    new logic

--- update_status.php
+++ update_status.php
@@ -8,31 +8,44 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $successResponse = ['status' => true, 'error' => null, 'users' => []];
     $errors = [];
 
+    $foundUsers = [];
+
     foreach ($users as $userData) {
         $userId = isset($userData['id']) ? $userData['id'] : null;
 
         if ($userId !== null) {
-            $updateQuery = "UPDATE `client` SET `status`='$newStatus' WHERE `id`='$userId'";
+            $selectQuery = "SELECT `status` FROM `client` WHERE `id`='$userId'";
+            $result = mysqli_query($induction, $selectQuery);
 
-            if (mysqli_query($induction, $updateQuery)) {
-                $selectQuery = "SELECT `status` FROM `client` WHERE `id`='$userId'";
-                $result = mysqli_query($induction, $selectQuery);
+            if ($result) {
+                $row = mysqli_fetch_assoc($result);
 
-                if ($result) {
-                    $row = mysqli_fetch_assoc($result);
-
-                    if ($row) {
-                        $status = $row['status'];
-                        $successResponse['users'][] = ['id' => $userId, 'status' => $status];
-                    } else {
-                        $errors[] = ['userId' => $userId, 'error' => 'User with id ' . $userId . ' not found'];
-                    }
+                if ($row) {
+                    $status = $row['status'];
+                    $foundUsers[] = ['id' => $userId, 'status' => $status];
+                } else {
+                    $errors[] = ['userId' => $userId, 'error' => 'User with id ' . $userId . ' not found'];
                 }
             } else {
-                $errors[] = ['userId' => $userId, 'error' => 'Error updating status'];
+                $errors[] = ['userId' => $userId, 'error' => 'Error querying user with id ' . $userId];
             }
         } else {
             $errors[] = ['error' => 'User id is missing'];
+        }
+    }
+
+    if (empty($errors)) {
+      
+        foreach ($foundUsers as $userData) {
+            $userId = $userData['id'];
+            $status = $userData['status'];
+
+            $updateQuery = "UPDATE `client` SET `status`='$newStatus' WHERE `id`='$userId'";
+            if (!mysqli_query($induction, $updateQuery)) {
+                $errors[] = ['userId' => $userId, 'error' => 'Error updating status'];
+            } else {
+                $successResponse['users'][] = ['id' => $userId, 'status' => $newStatus];
+            }
         }
     }
 
@@ -46,4 +59,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     http_response_code(400);
     echo json_encode(['status' => false, 'error' => ['code' => 105, 'message' => 'Invalid request method']]);
 }
+
 ?>
